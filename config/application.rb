9f48388eed25431cd747efda95e24bc186a44ab4
yerassyl <erasildias@gmail.com>
2016-02-16 12:35:47 +0600

    assets

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SecondOpinion
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.assets.initialize_on_precompile = false
 
   end
 end
