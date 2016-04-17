29d5c6f43032e813c71533bdf35e1ecae02a6974
yerassyl <erasildias@gmail.com>
2016-04-17 20:43:57 +0600

    header: profile link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,18 @@ module ApplicationHelper
     user.roles.first.name
   end
 
+  # HEADER HELPERS
+
+  # user profile depending on it's role
+  def user_profile
+    if current_user.has_role?(:admin)
+    elsif current_user.has_role?(:manager)
+    elsif current_user.has_role?(:client)
+    elsif current_user.has_role?(:doctor)
+        doctor_path(current_user.doctor.id)
+    else # patient
+
+    end
+  end
+
 end
